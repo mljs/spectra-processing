4ade9abfab595311bb282f181b727e5b845d4628
Luc Patiny <luc@patiny.com>
2019-07-26 18:36:55 +0200

    add .babelrc

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,8 @@
-import commonjs from 'rollup-plugin-commonjs';
-import resolve from 'rollup-plugin-node-resolve';
-import babel from 'rollup-plugin-babel';
-
 export default {
   input: 'src/index.js',
   output: {
     format: 'cjs',
     file: 'lib/index.js',
+    exports: 'named',
   },
-  plugins: [
-    commonjs(), // prise en charge de require
-    resolve(), // prise en charge des modules depuis node_modules
-    babel(), // transpilation
-  ],
 };
