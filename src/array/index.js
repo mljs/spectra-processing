77acdc28e18077489bcc706425bc5d3f74cad98f
Luc Patiny <luc@patiny.com>
2019-08-05 15:07:28 +0200

    add sortX

--- src/array/index.js
+++ src/array/index.js
@@ -1,7 +1,9 @@
 import { findClosestIndex } from './findClosestIndex';
 import { rotate } from './rotate';
+import { sortX } from './sortX';
 
 export const Array = {
   findClosestIndex,
   rotate,
+  sortX,
 };
--- src/xreim/index.js
+++ src/xreim/index.js
@@ -1,5 +1,7 @@
 import { zeroFilling } from './zeroFilling';
+import { sortX } from './sortX';
 
 export const XReIm = {
   zeroFilling,
+  sortX,
 };
