3a1f0eaa7dd4bb5af1ff7780c75c22da66d02cb3
Luc Patiny <luc@patiny.com>
2019-10-01 13:28:25 +0200

    feat: add X.boxPlot

--- src/x/index.js
+++ src/x/index.js
@@ -1,17 +1,19 @@
+import { add } from './add';
+import { boxPlot } from './boxPlot';
+import { divide } from './divide';
 import { findClosestIndex } from './findClosestIndex';
+import { getTargetIndex } from './getTargetIndex';
+import { multiply } from './multiply';
 import { rotate } from './rotate';
 import { subtract } from './subtract';
-import { add } from './add';
-import { multiply } from './multiply';
-import { divide } from './divide';
-import { getTargetIndex } from './getTargetIndex';
 
 export const X = {
+  add,
+  boxPlot,
+  divide,
   findClosestIndex,
+  getTargetIndex,
+  multiply,
   rotate,
   subtract,
-  add,
-  multiply,
-  divide,
-  getTargetIndex,
 };
