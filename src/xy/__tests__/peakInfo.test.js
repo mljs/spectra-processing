c76bfe6a526e26cc0231d0eddcf378a65b7c9c7e
Luc Patiny <luc@patiny.com>
2019-10-10 08:02:12 +0200

    feat: add peakInfo

---
+++ src/xy/__tests__/peakInfo.test.js
@@ -0,0 +1,29 @@
+import { peakInfo } from '../peakInfo.js';
+
+describe('peakInfo', function() {
+  it('positive peak', function() {
+    let x = [0, 1, 2, 3, 4, 5, 6];
+    let y = [1, 2, 3, 5, 3, 2, 1];
+    let result = peakInfo({ x, y }, { targetIndex: 3 });
+
+    expect(result).toStrictEqual({
+      inflectionBefore: { x: 2, y: 3 },
+      inflectionAfter: { x: 4, y: 3 },
+      extrema: { x: 3, y: 5 },
+      inflectionMiddle: { x: 3, y: 3 },
+      width: 2,
+    });
+  });
+  it('negative peak', function() {
+    let x = [0, 1, 2, 3, 4, 5, 6];
+    let y = [-1, -2, -3, -5, -3, -2, -1];
+    let result = peakInfo({ x, y }, { targetIndex: 3 });
+    expect(result).toStrictEqual({
+      inflectionBefore: { x: 2, y: -3 },
+      inflectionAfter: { x: 4, y: -3 },
+      extrema: { x: 3, y: -5 },
+      inflectionMiddle: { x: 3, y: -3 },
+      width: 2,
+    });
+  });
+});
