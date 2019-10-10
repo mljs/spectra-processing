f192ea7cb48d691dffa3b10569c37e3b6546bc1b
Luc Patiny <luc@patiny.com>
2019-10-10 20:20:52 +0200

    chore: circumvent eslint problem

--- src/xy/__tests__/maximaY.test.js
+++ src/xy/__tests__/maximaY.test.js
@@ -13,13 +13,15 @@ describe('maximaY', function () {
   it('flat top', function () {
     let x = [0, 1, 2, 3, 4, 5];
     let y = [1, 2, 3, 3, 2, 1];
-    expect(maximaY({ x, y }, { from: 0, to: 1 })).toStrictEqual([{ x: 2, y: 3, index: 2 },]);
+    let maximaY = maximaY({ x, y }, { from: 0, to: 1 });
+    expect(maximaY).toStrictEqual([{ x: 2, y: 3, index: 2 }]);
   });
 
   it('large flat top', function () {
     let x = [1, 2, 3, 4, 5, 6, 7, 8, 9];
     let y = [1, 2, 3, 3, 3, 3, 3, 2, 1];
-    expect(maximaY({ x, y }, { from: 0, to: 1 })).toStrictEqual([{ x: 5, y: 3, index: 4 },]);
+    let maximaY = maximaY({ x, y }, { from: 0, to: 1 });
+    expect(maximaY).toStrictEqual([{ x: 5, y: 3, index: 4 }]);
   });
 
   it('2 large flat top', function () {
