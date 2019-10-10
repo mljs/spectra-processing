abeca93104e150f538939406cc5a3f8529597941
Luc Patiny <luc@patiny.com>
2019-10-10 20:17:45 +0200

    chore: fix eslint

--- src/xy/__tests__/maxYPoint.test.js
+++ src/xy/__tests__/maxYPoint.test.js
@@ -1,13 +1,13 @@
 import { maxYPoint } from '../maxYPoint.js';
 
-describe('maxYPoint', function() {
-  it('no from to', function() {
+describe('maxYPoint', function () {
+  it('no from to', function () {
     let x = [0, 1, 2, 3];
     let y = [1, 2, 3, 1];
     expect(maxYPoint({ x, y })).toStrictEqual({ x: 2, y: 3 });
   });
 
-  it('with from to', function() {
+  it('with from to', function () {
     let x = [0, 1, 2, 3];
     let y = [1, 2, 3, 1];
     expect(maxYPoint({ x, y }, { from: 0, to: 1 })).toStrictEqual({
