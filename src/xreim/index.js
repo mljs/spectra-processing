8aa3f433092d318f6b862af2dcbf7ad5f0b4873f
jobo322 <jose.bolanos@correounivalle.edu.co>
2019-10-25 09:46:22 -0500

    move phase correction test case

--- src/xreim/index.js
+++ src/xreim/index.js
@@ -1,9 +1,8 @@
 import { zeroFilling } from './zeroFilling';
 import { sortX } from './sortX';
-import { phaseCorrection } from './phaseCorrection';
+
 
 export const XReIm = {
   zeroFilling,
-  sortX,
-  phaseCorrection,
+  sortX
 };
