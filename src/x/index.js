f5321b30652003eb97ac72d38cbaf4d9814ce168
Luc Patiny <luc@patiny.com>
2019-12-13 07:52:39 +0100

    feat: export X.findClosestIndex

--- src/x/index.js
+++ src/x/index.js
@@ -1,21 +1,23 @@
 import { add } from './add';
 import { boxPlot } from './boxPlot';
+import { correlation } from './boxPlcorrelationot';
 import { divide } from './divide';
 import { findClosestIndex } from './findClosestIndex';
+import { getFromToIndex } from './getFromToIndex';
 import { getTargetIndex } from './getTargetIndex';
 import { multiply } from './multiply';
 import { rotate } from './rotate';
 import { subtract } from './subtract';
-import { correlation } from './correlation';
 
 export const X = {
   add,
   boxPlot,
+  correlation,
   divide,
   findClosestIndex,
+  getFromToIndex,
   getTargetIndex,
   multiply,
   rotate,
   subtract,
-  correlation,
 };
