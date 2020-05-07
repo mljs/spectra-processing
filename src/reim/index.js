b180cc555fe55291fa75f5d96598c2b4b11ee9c1
jobo322 <jose.bolanos@correounivalle.edu.co>
2020-05-07 15:18:08 -0500

    feat: add automatic phase correction in ReIm

--- src/reim/index.js
+++ src/reim/index.js
@@ -1,7 +1,9 @@
 import { absolute } from './absolute';
 import { phaseCorrection } from './phaseCorrection';
+import { autoPhaseCorrection } from './autoPhaseCorrection';
 
 export const ReIm = {
   absolute,
   phaseCorrection,
+  autoPhaseCorrection,
 };
