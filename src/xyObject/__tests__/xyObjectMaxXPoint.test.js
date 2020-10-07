764c8416a2835c5fbdb9396cd76a6983f2006cfd
Luc Patiny <luc@patiny.com>
2020-10-07 14:37:07 +0200

    feat: add xyObjectMinXPoint and xyObjectMaxXPoint

---
+++ src/xyObject/__tests__/xyObjectMaxXPoint.test.js
@@ -0,0 +1,12 @@
+import { xyObjectMaxYPoint } from '../xyObjectMaxYPoint';
+
+test('xyObjectMaxYPoint', () => {
+  let arrayXY = [
+    { x: 1, y: 1 },
+    { x: 3, y: 3 },
+    { x: 2, y: 0 },
+    { x: 0, y: 2 },
+  ];
+
+  expect(xyObjectMaxYPoint(arrayXY)).toStrictEqual({ x: 3, y: 3, index: 1 });
+});
