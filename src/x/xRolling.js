eb398baf813e7aef33b4b104d92c17165080a8f1
Michaël Zasso <targos@protonmail.com>
2020-10-09 12:44:26 +0200

    fix: check that xRolling parameter is an array

--- src/x/xRolling.js
+++ src/x/xRolling.js
@@ -1,16 +1,18 @@
+import xCheck from './xCheck';
 import { xPadding } from './xPadding';
 /**
  * This function calculates a rolling average
  * @param {Array<Number>} array - the array that will be rotated
+ * @param {function} fct callback function that from an array returns a value.
  * @param {object} [options={}]
  * @param {number} [options.window=5] rolling window
- * @param {function} [fct] callback function that from an array returns a value.
  * @param {string} [options.padding.size=0] none, value, circular, duplicate
  * @param {string} [options.padding.algorithm='value'] none, value, circular, duplicate
  * @param {number} [options.padding.value=0] value to use for padding (if algorithm='value')
  * @return {Array<Number>}
  */
 export function xRolling(array, fct, options = {}) {
+  xCheck(array);
   if (typeof fct !== 'function') throw Error('fct has to be a function');
 
   const { window = 5, padding = {} } = options;
