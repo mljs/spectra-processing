32676ab12ea5ee70e8862dc0c4063dff90a470ee
Luc Patiny <lpatiny@users.noreply.github.com>
2020-10-09 12:32:21 +0200

    feat: add xRollingMedian (#21)

---
+++ src/x/__tests__/xRolling.test.js
@@ -0,0 +1,15 @@
+import { toMatchCloseTo } from 'jest-matcher-deep-close-to';
+
+import { xRolling } from '../xRolling.js';
+
+expect.extend({ toMatchCloseTo });
+
+test('xRolling', function () {
+  let array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+  expect(() => xRolling(array)).toThrow('fct has to be a function');
+  // prettier-ignore
+  expect(xRolling(array, () => 1, { window: 5 })).toStrictEqual([
+    1,1,1,1,1
+  ]);
+});
---
+++ src/x/__tests__/xRollingMedian.test.js
@@ -0,0 +1,22 @@
+import { toMatchCloseTo } from 'jest-matcher-deep-close-to';
+
+import { xRollingMedian } from '../xRollingMedian.js';
+
+expect.extend({ toMatchCloseTo });
+
+test('xRollingMedian', function () {
+  let array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+  expect(xRollingMedian(array)).toStrictEqual([3, 4, 5, 6, 7]);
+  // prettier-ignore
+  expect(xRollingMedian(array, { window: 3 })).toStrictEqual([
+    2, 3, 4, 5, 6, 7, 8,
+  ]);
+
+  // prettier-ignore
+  expect(xRollingMedian(array, { window: 3, padding: {
+    algorithm:'duplicate'
+  }})).toMatchCloseTo([
+    1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 9
+  ],5);
+});
