5891b8beace2ce68201013bb056e94fa8f70e425
josoriom <javier.osorio@correounivalle.edu.co>
2020-11-06 11:46:52 -0500

    improve: xNorm test case

--- src/x/__tests__/xNorm.test.js
+++ src/x/__tests__/xNorm.test.js
@@ -6,5 +6,9 @@ describe('xNorm', function () {
     expect(xNorm([-3, 4])).toStrictEqual(5);
     expect(xNorm([3, -4])).toStrictEqual(5);
     expect(xNorm([-3, -4])).toStrictEqual(5);
+    expect(xNorm([1, 2, 2])).toStrictEqual(3);
+    expect(xNorm([2, 3, 6])).toStrictEqual(7);
+    expect(xNorm([4, 4, 7])).toStrictEqual(9);
+    expect(xNorm([1, 4, 8])).toStrictEqual(9);
   });
 });
