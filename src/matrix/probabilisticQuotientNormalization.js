3bf51de78f8954ef5854d8d3ed33c6b0dbeeedd4
josoriom <javier.osorio@correounivalle.edu.co>
2020-11-11 12:05:19 -0500

    fix: add ml-array-mean

--- src/matrix/probabilisticQuotientNormalization.js
+++ src/matrix/probabilisticQuotientNormalization.js
@@ -1,55 +1,52 @@
+import mean from 'ml-array-mean';
 import { Matrix } from 'ml-matrix';
 
 /**
  * Performs a Probabilistic quotient normalization over the dataset to account dilution based in median spectrum.
  * Dieterle, F., Ross, A., Schlotterbeck, G., & Senn, H. (2006). Probabilistic quotient normalization as robust method to account for dilution of complex biological mixtures. Application in 1H NMR metabonomics. Analytical chemistry, 78(13), 4281-4290.
  * DOI: 10.1021/ac051632c
- * @param {Matrix} [dataset] - matrix [rows][cols].
- * @param {number} [options.max] - Normalization integral constant.
+ * @param {Matrix} [matrix] - matrix [rows][cols].
+ * @param {Object} [options={}]
+ * @param {number} [options.max=100] - Normalization integral constant.
  * @return {Object} { data, averageQuotients }.
  * data: Normalized dataset.
  * averageQuotients: The quotients of all variables of interest.
  */
-export function probabilisticQuotientNormalization(dataset, options = {}) {
+export function probabilisticQuotientNormalization(matrix, options = {}) {
   let { max = 100 } = options;
-  let matrixDataset = Matrix.checkMatrix(dataset.clone());
-  for (let i = 0; i < matrixDataset.rows; i++) {
-    let row = matrixDataset
-      .getRowVector(i)
-      .div(matrixDataset.getRowVector(i).norm() / max);
-    matrixDataset.setRow(i, row);
+  matrix = new Matrix(matrix);
+  for (let i = 0; i < matrix.rows; i++) {
+    let row = matrix.getRowVector(i).div(matrix.getRowVector(i).norm() / max);
+    matrix.setRow(i, row);
   }
-  let normalizationFactor = matrixDataset.norm() / max;
-  matrixDataset.div(normalizationFactor);
+  let normalizationFactor = matrix.norm() / max;
+  matrix.div(normalizationFactor);
   let referenceSpectrum = [];
-  let nbSamples = matrixDataset.rows;
-  for (let i = 0; i < matrixDataset.columns; i++) {
+  let nbSamples = matrix.rows;
+  for (let i = 0; i < matrix.columns; i++) {
     let currentVariable = [];
-    for (let j = 0; j < matrixDataset.rows; j++) {
-      currentVariable.push(matrixDataset.get(j, i));
+    for (let j = 0; j < matrix.rows; j++) {
+      currentVariable.push(matrix.get(j, i));
     }
     referenceSpectrum.push(
       currentVariable.reduce((prev, current) => prev + current) / nbSamples,
     );
   }
   let averageQuotients = [];
-  let nbVariables = matrixDataset.columns;
-  for (let i = 0; i < matrixDataset.rows; i++) {
+  for (let i = 0; i < matrix.rows; i++) {
     let quotients = [];
-    for (let j = 0; j < matrixDataset.columns; j++) {
-      let quotient = matrixDataset.get(i, j) / referenceSpectrum[j];
+    for (let j = 0; j < matrix.columns; j++) {
+      let quotient = matrix.get(i, j) / referenceSpectrum[j];
       quotients.push(quotient);
     }
-    averageQuotients.push(
-      quotients.reduce((prev, current) => prev + current) / nbVariables,
-    );
+    averageQuotients.push(mean(quotients));
   }
 
-  for (let i = 0; i < matrixDataset.rows; i++) {
-    matrixDataset.mulRow(i, averageQuotients[i]);
+  for (let i = 0; i < matrix.rows; i++) {
+    matrix.mulRow(i, averageQuotients[i]);
   }
   return {
-    data: matrixDataset,
+    data: matrix,
     averageQuotients: averageQuotients,
   };
 }
