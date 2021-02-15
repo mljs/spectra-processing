44c48022ee6618d191bf7c777fd19fe5ca375a25
Kevin M. Jablonka <kevin.jablonka@epfl.ch>
2021-02-15 19:24:45 +0100

    fix: xMonotone was failing if it started with constant values

--- src/x/xIsMonotone.js
+++ src/x/xIsMonotone.js
@@ -4,8 +4,13 @@
  * @return {boolean}
  */
 export function xIsMonotone(array) {
-  if (array.length < 3) return true;
-  if (array[0] < array[1]) {
+  if (array.length <= 2) {
+    return true;
+  }
+  if (array.every((val, i, arr) => val === arr[0])) {
+    return true;
+  }
+  if (array[0] < array[array.length - 1]) {
     for (let i = 0; i < array.length - 1; i++) {
       if (array[i] >= array[i + 1]) return false;
     }
