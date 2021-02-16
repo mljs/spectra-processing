3fbd82b9a335724bbada5343e9654836ab769e47
Kevin Jablonka <32935233+kjappelbaum@users.noreply.github.com>
2021-02-16 14:31:35 +0100

    fix: xMonotone was failing if it started with constant values (#54)
    * fix: xMonotone was failing if it started with constant values
    
    * choe: improve speed of xIsMonotone
    
    Co-authored-by: Luc Patiny <luc@patiny.com>

--- src/x/__tests__/xIsMonotone.test.js
+++ src/x/__tests__/xIsMonotone.test.js
@@ -11,6 +11,11 @@ describe('xIsMonotone', function () {
     expect(xIsMonotone(array)).toBe(false);
   });
 
+  it('test xIsMonotone increasing but duplicate starting value', () => {
+    let array = [1, 1, 2, 4, 4, 5];
+    expect(xIsMonotone(array)).toBe(false);
+  });
+
   it('test xIsMonotone increasing with mistake', () => {
     let array = [1, 2, 3, 5, 4];
     expect(xIsMonotone(array)).toBe(false);
@@ -26,8 +31,18 @@ describe('xIsMonotone', function () {
     expect(xIsMonotone(array)).toBe(false);
   });
 
+  it('test xIsMonotone decreasing but duplicate starting value', () => {
+    let array = [5, 5, 4, 4, 2, 1];
+    expect(xIsMonotone(array)).toBe(false);
+  });
+
   it('test xIsMonotone decreasing with mistake', () => {
     let array = [4, 5, 3, 2, 1];
     expect(xIsMonotone(array)).toBe(false);
+  });
+
+  it('test xIsMonotone constant series', () => {
+    let array = [1, 1, 1, 1];
+    expect(xIsMonotone(array)).toBe(true);
   });
 });
--- src/x/xIsMonotone.js
+++ src/x/xIsMonotone.js
@@ -4,8 +4,18 @@
  * @return {boolean}
  */
 export function xIsMonotone(array) {
-  if (array.length < 3) return true;
-  if (array[0] < array[1]) {
+  if (array.length <= 2) {
+    return true;
+  }
+  if (array[0] === array[1]) {
+    // maybe a constant series
+    for (let i = 1; i < array.length - 1; i++) {
+      if (array[i] !== array[i + 1]) return false;
+    }
+    return true;
+  }
+
+  if (array[0] < array[array.length - 1]) {
     for (let i = 0; i < array.length - 1; i++) {
       if (array[i] >= array[i + 1]) return false;
     }
