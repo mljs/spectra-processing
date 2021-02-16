efb60bfc9df4cd338734602f92395c7ad9e0f04c
Luc Patiny <luc@patiny.com>
2021-02-16 14:29:35 +0100

    choe: improve speed of xIsMonotone

--- src/x/xIsMonotone.js
+++ src/x/xIsMonotone.js
@@ -7,9 +7,14 @@ export function xIsMonotone(array) {
   if (array.length <= 2) {
     return true;
   }
-  if (array.every((val, i, arr) => val === arr[0])) {
+  if (array[0] === array[1]) {
+    // maybe a constant series
+    for (let i = 1; i < array.length - 1; i++) {
+      if (array[i] !== array[i + 1]) return false;
+    }
     return true;
   }
+
   if (array[0] < array[array.length - 1]) {
     for (let i = 0; i < array.length - 1; i++) {
       if (array[i] >= array[i + 1]) return false;
