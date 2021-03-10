fed46506b2da92792ef0fa3082bca2df81d3fb44
Luc Patiny <luc@patiny.com>
2021-03-10 13:55:15 +0100

    chore: add reference for xyQuickSortX

--- src/xy/xyQuickSortX.js
+++ src/xy/xyQuickSortX.js
@@ -1,5 +1,7 @@
 import { xyCheck } from './xyCheck';
 
+// based on https://code.woboq.org/userspace/glibc/stdlib/qsort.c.html#148
+
 /**
  *This function performs a quick sort of the x array while transforming the y array to preserve the coordinates.
  * @param {DataXY} [data] Object that contains property x (Array) and y (Array)
