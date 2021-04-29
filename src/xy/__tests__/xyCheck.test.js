d3219e5caed81d9348204de74f807b62435a225e
Luc Patiny <luc@patiny.com>
2021-04-29 07:39:30 +0200

    chore: fix error name

--- src/xy/__tests__/xyCheck.test.js
+++ src/xy/__tests__/xyCheck.test.js
@@ -2,8 +2,8 @@ import { xyCheck } from '../xyCheck.js';
 
 describe('xyCheck', () => {
   it('various kind of object', () => {
-    expect(() => xyCheck()).toThrow('Points must be an object');
-    expect(() => xyCheck({ x: [], z: [] })).toThrow('Points must be an object');
+    expect(() => xyCheck()).toThrow('Data must be an object');
+    expect(() => xyCheck({ x: [], z: [] })).toThrow('Data must be an object');
     expect(() => xyCheck({ x: [1], y: [1, 2] })).toThrow(
       'The x and y arrays mush',
     );
--- src/xy/xyCheck.js
+++ src/xy/xyCheck.js
@@ -6,7 +6,7 @@ import isAnyArray from 'is-any-array';
  */
 export function xyCheck(data = {}) {
   if (!isAnyArray(data.x) || !isAnyArray(data.y)) {
-    throw new Error('Points must be an object of x and y arrays');
+    throw new Error('Data must be an object of x and y arrays');
   }
   if (data.x.length !== data.y.length) {
     throw new Error('The x and y arrays mush have the same length');
