d987ac95488230fab109942a8cd39c751fb08016
Aiday Marlen Kyzy <aidaymarlenkyzy@gmail.com>
2022-01-20 11:04:21 +0100

    feat: added utility functions in order to encode textual features into numerical features  (#104)

---
+++ src/matrix/__tests__/matrixNumericalEncoding.test.ts
@@ -0,0 +1,39 @@
+import { matrixNumericalEncoding } from '../matrixNumericalEncoding';
+
+describe('matrixNumericalEncoding', () => {
+  let dataset = [
+    [73, 'a', 'o', 152],
+    [93, 'b', 'u', 185],
+    [89, 'c', 'p', 180],
+    [96, 'd', 'v', 196],
+    [73, 'e', 'j', 142],
+    [53, 'f', 'w', 101],
+    [69, 'g', 'x', 149],
+    [47, 'h', 'y', 115],
+    [87, 'i', 'p', 175],
+    [79, 'j', 'b', 164],
+    [69, 'j', 'm', 141],
+    [70, 'k', 'g', 141],
+    [93, 'l', 'c', 184],
+    [79, 'a', 'm', 152],
+    [70, 'm', 'z', 148],
+    [93, 'n', 'aa', 192],
+  ];
+
+  it('should return an array of numbers', () => {
+    let { matrix } = matrixNumericalEncoding(dataset);
+
+    function allNumbers(arrayData: (string | number)[][]) {
+      for (let subArray of arrayData) {
+        for (let element of subArray) {
+          if (typeof element === 'string') {
+            return false;
+          }
+        }
+      }
+      return true;
+    }
+
+    expect(allNumbers(matrix)).toBe(true);
+  });
+});
---
+++ src/matrix/matrixNumericalEncoding.ts
@@ -0,0 +1,41 @@
+import { matrixClone } from './matrixClone';
+
+/**
+ * Numerically encodes the strings in the matrix and returns an encoding dictionnary which can be used to encode other matrices
+ * @param matrix - original matrix before encoding
+ * @returns - dictionnary from string to number
+ */
+export function matrixNumericalEncoding(matrixInitial: (string | number)[][]): {
+  matrix: number[][];
+  dictCategoricalToNumerical: { [stringValue: string]: number };
+} {
+  let matrix = matrixClone(matrixInitial);
+  let nRows = matrix.length;
+  let nColumns = matrix[0].length;
+
+  let dictCategoricalToNumerical: { [stringValue: string]: number } = {};
+  let k = 0;
+
+  for (let i = 0; i < nRows; i++) {
+    for (let j = 0; j < nColumns; j++) {
+      if (typeof matrix[i][j] === 'number' && matrix[i][j] > k) {
+        k = matrix[i][j] as number;
+      }
+    }
+  }
+
+  for (let i = 0; i < nRows; i++) {
+    for (let j = 0; j < nColumns; j++) {
+      if (typeof matrix[i][j] === 'string') {
+        if (matrix[i][j] in dictCategoricalToNumerical) {
+          matrix[i][j] = dictCategoricalToNumerical[matrix[i][j]];
+        } else {
+          k++;
+          dictCategoricalToNumerical[matrix[i][j]] = k;
+          matrix[i][j] = k;
+        }
+      }
+    }
+  }
+  return { matrix, dictCategoricalToNumerical };
+}
