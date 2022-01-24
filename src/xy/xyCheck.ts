06501dfd989cbe1cc799485492bd03271bf67871
Luc Patiny <luc@patiny.com>
2022-01-24 13:24:44 +0100

    feat: xyCheck may have the option minLength

--- src/xy/xyCheck.ts
+++ src/xy/xyCheck.ts
@@ -1,17 +1,21 @@
 import { isAnyArray } from 'is-any-array';
 
-import { DataXYZ } from '..';
-
 /**
  * Throw an error in no an object of x,y arrays
  *
  * @param data - array of points {x,y,z}
  */
-export function xyCheck(data: DataXYZ = {}) {
-  if (!isAnyArray(data.x) || !isAnyArray(data.y)) {
+export function xyCheck(data: any, options: { minLength?: number } = {}) {
+  const { minLength } = options;
+  if (typeof data !== 'object' || !isAnyArray(data.x) || !isAnyArray(data.y)) {
     throw new Error('Data must be an object of x and y arrays');
   }
   if ((data.x as number[]).length !== (data.y as number[]).length) {
-    throw new Error('The x and y arrays mush have the same length');
+    throw new Error('The x and y arrays must have the same length');
+  }
+  if (minLength) {
+    if (data.x.length < minLength) {
+      throw new Error(`data.x must have a length of at least ${minLength}`);
+    }
   }
 }
