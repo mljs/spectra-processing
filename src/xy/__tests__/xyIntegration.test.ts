cdbe86efcfdd0a77b3027a8568ba69a388d5e514
Luc Patiny <lpatiny@users.noreply.github.com>
2022-01-26 15:29:03 +0100

    fix: xyIntegral and xyIntegration throw error is length 0 and returns correct value if length=1 (#110)
    close https://github.com/mljs/spectra-processing/issues/109

--- src/xy/__tests__/xyIntegration.test.ts
+++ src/xy/__tests__/xyIntegration.test.ts
@@ -1,6 +1,21 @@
 import { xyIntegration } from '../xyIntegration';
 
 describe('XY xyIntegration', () => {
+  it('zero element', () => {
+    let x: number[] = [];
+    let y: number[] = [];
+    expect(() => xyIntegration({ x, y })).toThrow(
+      'data.x must have a length of at least 1',
+    );
+  });
+
+  it('one element', () => {
+    let x = [1];
+    let y = [2];
+    let result = xyIntegration({ x, y });
+    expect(result).toBe(0);
+  });
+
   it('no from to', () => {
     let x = [0, 1, 2, 3];
     let y = [1, 1, 1, 1];
