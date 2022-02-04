ce931d9a677ceb271b20f58a3e9732a54e984130
aiday-mar <aidaymarlenkyzy@gmail.com>
2022-02-04 15:30:40 +0100

    chore: cleaning code

--- src/utils/sort.ts
+++ src/utils/sort.ts
@@ -1,24 +1,23 @@
-
 /** Function that sorts arrays or Float64Arrays/Float32Arrays in ascending order
  * @param array - array to sort
  * @returns sorted array
-*/
-export function sortAscending(array : any) {
+ */
+export function sortAscending(array: any) {
   if (array instanceof Float32Array || array instanceof Float64Array) {
-      return array.sort();
-  } else if(array instanceof Array) {
-      return array.sort((a, b) => (a > b ? 1 : -1));
+    return array.sort();
+  } else if (array instanceof Array) {
+    return array.sort((a, b) => (a > b ? 1 : -1));
   }
 }
 
 /** Function that sorts arrays or Float64Arrays/Float32Arrays in descending order
  * @param array - array to sort
  * @returns sorted array
-*/
-export function sortDescending(array : any) {
+ */
+export function sortDescending(array: any) {
   if (array instanceof Float32Array || array instanceof Float64Array) {
-      return array.sort().reverse();
-  } else if(array instanceof Array) {
-      return array.sort((a, b) => (a > b ? -1 : 1));
+    return array.sort().reverse();
+  } else if (array instanceof Array) {
+    return array.sort((a, b) => (a > b ? -1 : 1));
   }
 }
