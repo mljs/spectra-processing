75e1fa4b7022686734c5100a2f057a748778e929
aiday-mar <aidaymarlenkyzy@gmail.com>
2022-02-15 12:57:14 +0100

    close: https://github.com/mljs/spectra-processing/issues/127

---
+++ src/utils/__tests__/numberSort.test.ts
@@ -0,0 +1,20 @@
+import { toBeDeepCloseTo } from 'jest-matcher-deep-close-to';
+import binarySearch from 'binary-search';
+
+import { numberSortAscending, numberSortDescending } from '../numberSort';
+
+expect.extend({ toBeDeepCloseTo });
+
+let x = [0, 3, 4, 2, 6]; // sorted : [0, 2, 3, 4, 6]
+
+describe('numberSort', () => {
+  it('testing numberSortDescending', () => {
+    let index = binarySearch(x, x[2], numberSortDescending);
+    expect(index).toBeDeepCloseTo(2);
+  });
+
+  it('testing numberSortAscending', () => {
+    let index = binarySearch(x, x[2], numberSortAscending);
+    expect(index).toBeDeepCloseTo(2);
+  });
+});
