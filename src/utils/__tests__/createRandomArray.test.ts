5a1abf3bbf44833489214ea3759017c37d99a1f0
Aiday Marlen Kyzy <aidaymarlenkyzy@gmail.com>
2022-02-16 07:29:08 +0100

    test: check if normal noise is normal ...
    close https://github.com/mljs/spectra-processing/issues/131

--- src/utils/__tests__/createRandomArray.test.ts
+++ src/utils/__tests__/createRandomArray.test.ts
@@ -1,4 +1,5 @@
 import { toBeDeepCloseTo } from 'jest-matcher-deep-close-to';
+import { optimize } from 'ml-spectra-fitting';
 
 import { xHistogram } from '../../x/xHistogram';
 import { createRandomArray } from '../createRandomArray';
@@ -13,8 +14,9 @@ describe('createRandomXArray', () => {
       length: 5,
       seed: 0,
     });
-    expect(array).toBeDeepCloseTo([10, 10, 10, 10, 10]);
+    expect(array).toBeDeepCloseTo([10, 10, 10, 10, 10], 1);
   });
+
   it('uniform distribution', () => {
     let array = createRandomArray({
       mean: 10,
@@ -32,5 +34,26 @@ describe('createRandomXArray', () => {
       expect(y).toBeGreaterThan(9500);
       expect(y).toBeLessThan(10500);
     }
+  });
+
+  it('Testing in conjunction with spectra-fitting', () => {
+    let array = createRandomArray({
+      mean: 10,
+      standardDeviation: 1,
+      length: 100000,
+      distribution: 'normal',
+    });
+    const histogram = xHistogram(array, { centerX: false });
+    let fittedPeaks = optimize(
+      histogram,
+      [{ x: 10, y: 0.1, width: 2, fwhm: 2 }],
+      { shape: { kind: 'gaussian' } },
+    );
+    expect(fittedPeaks.peaks[0].x).toBeDeepCloseTo(10, 2);
+    expect(fittedPeaks.peaks[0].width).toBeDeepCloseTo(2, 2);
+    expect(fittedPeaks.peaks[0].fwhm).toBeDeepCloseTo(
+      2 * Math.sqrt(2 * Math.log(2)),
+      1,
+    );
   });
 });
