a809c7e78ae7108b09ef5d1c00054c2129321f92
Luc Patiny <luc@patiny.com>
2022-04-01 16:36:56 +0200

    chore: simplify subarray

--- src/x/xRolling.ts
+++ src/x/xRolling.ts
@@ -51,9 +51,8 @@ export function xRolling(
 
   const newArray = [];
   for (let i = 0; i < array.length - window + 1; i++) {
-    let subArray = new Float64Array(array.buffer, i * 8, window);
     // we will send a view to the original buffer
-    newArray.push(fct(subArray));
+    newArray.push(fct(array.subarray(i, i + window)));
   }
 
   return newArray;
