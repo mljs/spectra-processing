0ee8d45bed2b868f0687b8af77604b532d2495a2
EscapedGibbon <maximgolubev2@gmail.com>
2022-04-06 14:29:46 +0200

    docs: slight changes in "options" documentation

--- src/x/xMinIndex.ts
+++ src/x/xMinIndex.ts
@@ -6,7 +6,8 @@ import { xGetFromToIndex, XGetFromToIndexOptions } from './xGetFromToIndex';
  * Computes the index of the minimum of the given values
  *
  * @param array - array of numbers
- * @param options - options
+ * @param options - allows to apply the algorithm on a part of the array
+ * @returns - index
  */
 export function xMinIndex(
   array: NumberArray,
