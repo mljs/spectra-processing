a11cd4264b597224dde11c4948fee98d5a45918a
Luc Patiny <luc@patiny.com>
2023-02-07 13:21:10 +0100

    feat: create xy2ToXY

---
+++ src/xy2/xy2ToXY.ts
@@ -0,0 +1,16 @@
+import { DataXY } from 'cheminfo-types';
+
+/**
+ * Convert an array of XY arrays to a DataXY object containing x,y arrays
+ *
+ * @param data - array of arrays [[x,y],[x,y],...]
+ */
+export function xy2ToXY(data: [number, number][]): DataXY<number[]> {
+  const xy2: DataXY<number[]> = { x: [], y: [] };
+
+  for (let xyValue of data) {
+    xy2.x.push(xyValue[0]);
+    xy2.y.push(xyValue[1]);
+  }
+  return xy2;
+}
