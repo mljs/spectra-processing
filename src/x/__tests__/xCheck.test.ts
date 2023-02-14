231ca46a8e5e0b96d932bd9ec607eb705e05ffa8
Luc Patiny <luc@patiny.com>
2023-02-14 11:01:12 +0100

    feat: xCheck as a minLength

--- src/x/__tests__/xCheck.test.ts
+++ src/x/__tests__/xCheck.test.ts
@@ -1,8 +1,12 @@
 import { xCheck } from '../xCheck';
 
-describe('test xcheck', () => {
+describe('test xCheck', () => {
   it('should throw on invalid value', () => {
     expect(() => xCheck()).toThrow(/input must be an array/);
     expect(() => xCheck([])).toThrow(/input must not be empty/);
+    expect(() => xCheck([1])).toBeTruthy();
+    expect(() => xCheck([1], { minLength: 2 })).toThrow(
+      /input must have a length of at least 2/,
+    );
   });
 });
--- src/x/xCheck.ts
+++ src/x/xCheck.ts
@@ -6,12 +6,22 @@ import { isAnyArray } from 'is-any-array';
  *
  * @param input - input
  */
-export function xCheck(input?: NumberArray) {
+export function xCheck(
+  input?: NumberArray,
+  options: {
+    /** minimum length */
+    minLength?: number;
+  } = {},
+) {
+  const { minLength } = options;
   if (!isAnyArray(input)) {
     throw new TypeError('input must be an array');
   }
-
   if ((input as NumberArray).length === 0) {
     throw new TypeError('input must not be empty');
+  }
+  //@ts-expect-error we already checked that input is an array
+  if (minLength && input.length < minLength) {
+    throw new Error(`input must have a length of at least ${minLength}`);
   }
 }
