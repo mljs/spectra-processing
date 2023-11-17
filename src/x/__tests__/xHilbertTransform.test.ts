10523596f1a4d6c8243491cc0e25ac8df9ab96b1
josoriom <josoriom93@gmail.com>
2023-11-17 14:39:49 -0500

    refactor!: perform the hilbert transform using the fft

--- src/x/__tests__/xHilbertTransform.test.ts
+++ src/x/__tests__/xHilbertTransform.test.ts
@@ -1,34 +1,32 @@
 import { xHilbertTransform } from '../../index';
 
-describe('test hilbert transform of cos(t)', () => {
-  const length = 50;
+describe('test hilbert transform', () => {
+  const length = 2 ** 6;
   it('test hilbert transform of cos -> sin function', () => {
     const cos = new Array(length)
       .fill(0)
-      .map((_, i) => Math.cos((i * Math.PI) / 10));
+      .map((_, i) => Math.cos((i * Math.PI) / 32));
     const sin = new Array(length)
       .fill(0)
-      .map((_, i) => Math.sin((i * Math.PI) / 10));
-    const trs = xHilbertTransform(cos);
-    const result = Array.from(trs);
-    // Excluding some points due to the edge effects
-    for (let i = 5; i < 45; i++) {
-      expect(result[i]).toBeCloseTo(sin[i], 1);
+      .map((_, i) => Math.sin((i * Math.PI) / 32));
+    const t = xHilbertTransform(cos);
+    const result = Array.from(t.im);
+    for (let i = 0; i < 64; i++) {
+      expect(result[i]).toBeCloseTo(sin[i], 6);
     }
   });
 
   it('test hilbert transform of sin -> -cos function', () => {
     const minusCos = new Array(length)
       .fill(0)
-      .map((_, i) => -Math.cos((i * Math.PI) / 10));
+      .map((_, i) => -Math.cos((i * Math.PI) / 32));
     const sin = new Array(length)
       .fill(0)
-      .map((_, i) => Math.sin((i * Math.PI) / 10));
-    const trs = xHilbertTransform(sin);
-    const result = Array.from(trs);
-    // Excluding some points due to the edge effects
-    for (let i = 15; i < 35; i++) {
-      expect(result[i]).toBeCloseTo(minusCos[i], 1);
+      .map((_, i) => Math.sin((i * Math.PI) / 32));
+    const t = xHilbertTransform(sin);
+    const result = Array.from(t.im);
+    for (let i = 0; i < 64; i++) {
+      expect(result[i]).toBeCloseTo(minusCos[i], 6);
     }
   });
 });
--- src/x/xHilbertTransform.ts
+++ src/x/xHilbertTransform.ts
@@ -1,28 +1,32 @@
+import FFT from 'fft.js';
+
 /**
  * Performs the Hilbert transform
  * @returns A new vector with 90 degree shift regarding the phase of the original function
  */
 
-export function xHilbertTransform(
-  input: number[],
-  options: { inClockwise?: boolean } = {},
-) {
-  const { inClockwise = true } = options;
-  const array = [0, ...input, 0];
-  const result = new Float64Array(input.length);
-  for (let k = 1; k < array.length - 1; k++) {
-    let aSum = 0;
-    for (let i = 0; i < k - 1; i++) {
-      const log = Math.log((k - i) / (k - i - 1));
-      aSum += array[i] * log + (array[i + 1] - array[i]) * (-1 + (k - i) * log);
-    }
-    const b = array[k - 1] - array[k + 1];
-    let cSum = 0;
-    for (let i = k + 1; i < array.length - 1; i++) {
-      const log = Math.log((i - k) / (i - k + 1));
-      cSum += array[i] * log + (array[i - 1] - array[i]) * (1 + (i - k) * log);
-    }
-    result[k - 1] = ((inClockwise ? 1 : -1) * (aSum + b + cSum)) / Math.PI;
+export function xHilbertTransform(x: number[]) {
+  if (!Array.isArray(x)) {
+    throw new Error('x must be a vector');
+  }
+  const n = x.length;
+  const fft = new FFT(n);
+  const v = [1, ...Array(n / 2 - 1).fill(2), 1, ...Array(n / 2 - 1).fill(0)];
+  const X = new Float64Array(n * 2);
+  fft.realTransform(X, x);
+  for (let i = 0; i < n; i++) {
+    X[i * 2] = X[i * 2] * v[i];
+    X[i * 2 + 1] = X[i * 2 + 1] * v[i];
+  }
+  const y = new Float64Array(n * 2);
+  fft.inverseTransform(y, X);
+  const result: { re: Float64Array; im: Float64Array } = {
+    re: new Float64Array(n),
+    im: new Float64Array(n),
+  };
+  for (let i = 0; i < n; i++) {
+    result.re[i] = y[i * 2];
+    result.im[i] = y[i * 2 + 1];
   }
   return result;
 }
