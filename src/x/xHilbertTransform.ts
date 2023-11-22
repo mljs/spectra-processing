8f9e905917cd6575bc8d21244a4a2bb2e752548d
josoriom <josoriom93@gmail.com>
2023-11-21 21:10:41 -0500

    chore: clean up the code and reuse package types

--- src/x/xHilbertTransform.ts
+++ src/x/xHilbertTransform.ts
@@ -1,59 +1,66 @@
+import { DoubleArray } from 'cheminfo-types';
 import FFT from 'fft.js';
-import { isAnyArray } from 'is-any-array';
+
+import { xCheck } from './xCheck';
 
 /**
  * Performs the Hilbert transform
  * @returns A new vector with 90 degree shift regarding the phase of the original function
  */
 
-export function xHilbertTransform(x: number[] | Float64Array) {
-  if (Math.log2(x.length) % 1 === 0) {
-    return hilbertTransformWithFFT(x);
+export function xHilbertTransform(array: DoubleArray) {
+  xCheck(array);
+  if (Math.log2(array.length) % 1 === 0) {
+    return hilbertTransformWithFFT(array);
   } else {
-    return hilbertTransform(x);
+    return hilbertTransform(array);
   }
 }
 
-export function hilbertTransformWithFFT(x: number[] | Float64Array) {
-  if (!isAnyArray(x)) {
-    throw new Error('x must be a vector');
-  }
-  const n = x.length;
+export function hilbertTransformWithFFT(array: DoubleArray) {
+  const n = array.length;
   const fft = new FFT(n);
-  const v = [1, ...Array(n / 2 - 1).fill(2), 1, ...Array(n / 2 - 1).fill(0)];
-  const X = new Float64Array(n * 2);
-  fft.realTransform(X, x);
+  const complexSignal = new Float64Array(n * 2);
   for (let i = 0; i < n; i++) {
-    X[i * 2] = X[i * 2] * v[i];
-    X[i * 2 + 1] = X[i * 2 + 1] * v[i];
+    complexSignal[i * 2] = array[i];
   }
-  const y = new Float64Array(n * 2);
-  fft.inverseTransform(y, X);
+  const fftResult = new Float64Array(n * 2);
+  fft.transform(fftResult, complexSignal);
+  const multiplier = new Float64Array(n);
+  for (let i = 1; i < n; i++) {
+    multiplier[i] = Math.sign(n / 2 - i);
+  }
+  for (let i = 0; i < n; i++) {
+    fftResult[i * 2] *= multiplier[i];
+    fftResult[i * 2 + 1] *= multiplier[i];
+  }
+  const hilbertSignal = new Float64Array(n * 2);
+  fft.inverseTransform(hilbertSignal, fftResult);
   const result = new Float64Array(n);
   for (let i = 0; i < n; i++) {
-    result[i] = y[i * 2 + 1];
+    result[i] = hilbertSignal[i * 2 + 1];
   }
   return result;
 }
 
 export function hilbertTransform(
-  input: number[] | Float64Array,
+  array: DoubleArray,
   options: { inClockwise?: boolean } = {},
 ) {
   const { inClockwise = true } = options;
-  const array = [0, ...input, 0];
-  const result = new Float64Array(input.length);
-  for (let k = 1; k < array.length - 1; k++) {
+  const input = [0, ...array, 0];
+  const result = new Float64Array(array.length);
+  for (let k = 1; k < input.length - 1; k++) {
     let aSum = 0;
     for (let i = 0; i < k - 1; i++) {
       const log = Math.log((k - i) / (k - i - 1));
-      aSum += array[i] * log + (array[i + 1] - array[i]) * (-1 + (k - i) * log);
+      aSum += input[i] * log + (input[i + 1] - input[i]) * (-1 + (k - i) * log);
     }
-    const b = array[k - 1] - array[k + 1];
+    const b = input[k - 1] - input[k + 1];
     let cSum = 0;
-    for (let i = k + 1; i < array.length - 1; i++) {
+    for (let i = k + 1; i < input.length - 1; i++) {
       const log = Math.log((i - k) / (i - k + 1));
-      cSum += array[i] * log + (array[i - 1] - array[i]) * (1 + (i - k) * log);
+      cSum += input[i] * log + (input[i - 1] - input[i]) * (1 + (i - k) * log);
     }
     result[k - 1] = ((inClockwise ? 1 : -1) * (aSum + b + cSum)) / Math.PI;
   }
