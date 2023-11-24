94120b011c04cf72a9db89380e48601c86f3ac67
josoriom <josoriom93@gmail.com>
2023-11-23 21:13:43 -0500

    test: add testcase to xResampling

---
+++ src/x/__tests__/xResampling.test.ts
@@ -0,0 +1,44 @@
+import { xResampling, xFindClosestIndex } from '..';
+
+describe('test resampling', () => {
+  const length = 16;
+  const x = new Float64Array(length);
+  const cos = new Float64Array(length);
+  const sin = new Float64Array(length);
+  const t = 8;
+  for (let i = 0; i < length; i++) {
+    x[i] = i;
+    cos[i] = Math.cos((i * Math.PI) / (t / 2));
+    sin[i] = Math.sin((i * Math.PI) / (t / 2));
+  }
+  it('Test reducing the size of the vector to half', () => {
+    const nLength = length / 2;
+    const nx = xResampling(x, nLength);
+    const ncos = xResampling(cos, nLength);
+    const nsin = xResampling(sin, nLength);
+    const points = [0, 0.25, 0.5, 0.75, 1, 1.25, 1.5, 1.75];
+    for (const point of points) {
+      expect(cos[xFindClosestIndex(x, t * point)]).toStrictEqual(
+        ncos[xFindClosestIndex(nx, t * point)],
+      );
+      expect(sin[xFindClosestIndex(x, t * point)]).toStrictEqual(
+        nsin[xFindClosestIndex(nx, t * point)],
+      );
+    }
+  });
+
+  it('Test increasing the size of the vector to twice its original size', () => {
+    const nLength = length * 2;
+    const nx = xResampling(x, nLength);
+    const ncos = xResampling(cos, nLength);
+    const nsin = xResampling(sin, nLength);
+    for (const point of x) {
+      expect(cos[xFindClosestIndex(x, t * point)]).toStrictEqual(
+        ncos[xFindClosestIndex(nx, t * point)],
+      );
+      expect(sin[xFindClosestIndex(x, t * point)]).toStrictEqual(
+        nsin[xFindClosestIndex(nx, t * point)],
+      );
+    }
+  });
+});
