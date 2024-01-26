922e2ccffb9c92fd39331eee574eaf236cec9572
Jose Alejandro Bolanos Arroyave <jose.bolanos@correounivalle.edu.co>
2024-01-26 01:31:57 -0500

    feat: add matrixSetSubMatrix (#211)
    * feat: add matrixSetSubMatrix
    
    * chore: test of the throw

---
+++ src/matrix/__tests__/matrixSetSubMatrix.test.ts
@@ -0,0 +1,31 @@
+import { matrixSetSubMatrix } from '../matrixSetSubMatrix';
+
+test('simple case', () => {
+  const matrix = [
+    [0, 1, 2, 3, 4],
+    [5, 6, 7, 8, 9],
+    [2, 3, 4, 5, 6],
+    [3, 4, 5, 6, 7],
+  ];
+
+  const subMatrix = [
+    [0, 0],
+    [0, 0],
+  ];
+
+  matrixSetSubMatrix(matrix, subMatrix, 2, 3);
+  expect(matrix).toStrictEqual([
+    [0, 1, 2, 3, 4],
+    [5, 6, 7, 8, 9],
+    [2, 3, 4, 0, 0],
+    [3, 4, 5, 0, 0],
+  ]);
+
+  expect(() => matrixSetSubMatrix(matrix, subMatrix, 2, 4)).toThrow(
+    'Submatrix indices are out of range',
+  );
+
+  expect(() =>
+    matrixSetSubMatrix(matrix, subMatrix.concat([0, 0]), 2, 3),
+  ).toThrow('Submatrix indices are out of range');
+});
