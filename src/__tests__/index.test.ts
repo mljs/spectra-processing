13c2c46df71670b4a7754623da64a29c1a12b45d
Michaël Zasso <targos@protonmail.com>
2024-02-26 08:36:43 +0100

    test: check all functions exported by index

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,12 +1,6 @@
-import * as SpectraProcessing from '..';
+import * as SpectraProcessing from '../index';
 
-describe('test existence of functions', () => {
-  it('x', () => {
-    expect(SpectraProcessing.xFindClosestIndex).toBeInstanceOf(Function);
-  });
-
-  it('xy', () => {
-    expect(SpectraProcessing.xyIntegral).toBeInstanceOf(Function);
-    expect(SpectraProcessing.xyIntegration).toBeInstanceOf(Function);
-  });
+it('test existence of exported functions', () => {
+  const exports = Object.keys(SpectraProcessing);
+  expect(exports).toMatchSnapshot();
 });
