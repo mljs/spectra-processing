3c72b05feb6eb005b7e24f93c772e5aef3db748c
Michaël Zasso <targos@protonmail.com>
2024-02-26 13:06:10 +0100

    fix!: improve types in zones functions

--- src/zones/zonesWithPoints.ts
+++ src/zones/zonesWithPoints.ts
@@ -1,4 +1,21 @@
+import { FromTo } from 'cheminfo-types';
+
 import { zonesNormalize } from './zonesNormalize';
+
+export interface ZonesWithPointsOptions {
+  /** specify min value of zones
+   * @default Number.NEGATIVE_INFINITY
+   */
+  from?: number;
+  /** specify max value of zones
+   * @default Number.POSITIVE_INFINITY
+   */
+  to?: number;
+}
+
+export interface FromToWithNumberOfPoints extends FromTo {
+  numberOfPoints: number;
+}
 
 /**
  * Add the number of points per zone to reach a specified total
@@ -9,49 +26,42 @@ import { zonesNormalize } from './zonesNormalize';
  * @returns array of zones with points
  */
 export function zonesWithPoints(
-  zones: Array<{
-    /** start of zone*/
-    from: number;
-    /** end of zone */
-    to: number;
-  }> = [],
+  zones: FromTo[] = [],
   /**
    * total number of points to distribute between zones
    * @default 10
    */
   numberOfPoints = 10,
-  options: {
-    /** specify min value of zones
-     * @default Number.NEGATIVE_INFINITY
-     */
-    from?: number;
-    /** specify max value of zones
-     * @default Number.POSITIVE_INFINITY
-     */
-    to?: number;
-  } = {},
-): Array<{ from: number; to: number; numberOfPoints?: number }> {
-  if (zones.length === 0) return zones;
-  const returnZones = zonesNormalize(zones, options);
+  options: ZonesWithPointsOptions = {},
+): FromToWithNumberOfPoints[] {
+  if (zones.length === 0) return [];
+  const normalizedZones = zonesNormalize(zones, options);
+  const zonesWithNumberOfPoints: FromToWithNumberOfPoints[] = [];
 
-  const totalSize = returnZones.reduce((previous, current) => {
+  const totalSize = normalizedZones.reduce((previous, current) => {
     return previous + (current.to - current.from);
   }, 0);
 
   const unitsPerPoint = totalSize / numberOfPoints;
   let currentTotal = 0;
 
-  for (let i = 0; i < returnZones.length - 1; i++) {
-    const tempZone: any = returnZones[i];
-    tempZone.numberOfPoints = Math.min(
+  for (let i = 0; i < normalizedZones.length - 1; i++) {
+    const tempZone = normalizedZones[i];
+    const tempZoneNumberOfPoints = Math.min(
       Math.round((tempZone.to - tempZone.from) / unitsPerPoint),
       numberOfPoints - currentTotal,
     );
-    currentTotal += tempZone.numberOfPoints;
+    zonesWithNumberOfPoints.push({
+      ...tempZone,
+      numberOfPoints: tempZoneNumberOfPoints,
+    });
+    currentTotal += tempZoneNumberOfPoints;
   }
 
-  const zone: any = returnZones[returnZones.length - 1];
-  zone.numberOfPoints = numberOfPoints - currentTotal;
+  zonesWithNumberOfPoints.push({
+    ...normalizedZones[normalizedZones.length - 1],
+    numberOfPoints: numberOfPoints - currentTotal,
+  });
 
-  return returnZones;
+  return zonesWithNumberOfPoints;
 }
