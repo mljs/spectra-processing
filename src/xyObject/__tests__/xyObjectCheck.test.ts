2f4de0c628fe45302ddc3cef7ff81f039d5426e8
Luc Patiny <luc@patiny.com>
2024-02-27 09:17:22 +0100

    test: add xyObjectCheck test

---
+++ src/xyObject/__tests__/xyObjectCheck.test.ts
@@ -0,0 +1,13 @@
+import { xyObjectCheck } from '../xyObjectCheck';
+
+test('xyObjectCheck', () => {
+  expect(xyObjectCheck([{ x: 0, y: 1 }])).toBeUndefined();
+  // @ts-expect-error this is a testcase
+  expect(() => xyObjectCheck([{ x: 0, y: 'a' }])).toThrow(
+    'points must be an array of {x,y} objects',
+  );
+  // @ts-expect-error this is a testcase
+  expect(() => xyObjectCheck('a')).toThrow(
+    'points must be an array of {x,y} objects',
+  );
+});
