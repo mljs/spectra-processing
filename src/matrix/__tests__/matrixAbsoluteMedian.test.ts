9af31e53489935a48dfbbeccde5ff7d3f7d0ea44
Luc Patiny <luc@patiny.com>
2024-02-27 09:09:48 +0100

    test: add test for matrixAbsoluteMedian

---
+++ src/matrix/__tests__/matrixAbsoluteMedian.test.ts
@@ -0,0 +1,10 @@
+import { matrixAbsoluteMedian } from '../matrixAbsoluteMedian';
+
+test('matrixAbsoluteMedian', () => {
+  const matrix = [
+    [-1, -2, -3],
+    [1, 2, 3],
+  ];
+  const absoluteMedian = matrixAbsoluteMedian(matrix);
+  expect(absoluteMedian).toBe(2);
+});
