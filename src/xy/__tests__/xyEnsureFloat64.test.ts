467a03a2aa55fe0c0b941613db519fedc95186f2
Jose Alejandro Bolanos Arroyave <jose.bolanos@correounivalle.edu.co>
2024-03-06 09:02:38 -0500

    feat: add xyEnsureFloat64 function (#224)

---
+++ src/xy/__tests__/xyEnsureFloat64.test.ts
@@ -0,0 +1,29 @@
+import { xyEnsureFloat64 } from '../xyEnsureFloat64';
+
+test('normal array', () => {
+  const data = {
+    x: [-1, 2, -3, 4],
+    y: [-1, 2, -3, 4],
+  };
+  const float64 = xyEnsureFloat64(data);
+  float64.x[0] = 0;
+  expect(float64.x).toBeInstanceOf(Float64Array);
+  expect(float64.y).toBeInstanceOf(Float64Array);
+  expect(data.x).toStrictEqual([-1, 2, -3, 4]);
+  expect(float64.x).toStrictEqual(Float64Array.from([0, 2, -3, 4]));
+  expect(float64.y).toStrictEqual(Float64Array.from([-1, 2, -3, 4]));
+});
+
+test('typed array', () => {
+  const data = {
+    x: Float64Array.from([-1, 2, -3, 4]),
+    y: Float64Array.from([-1, 2, -3, 4]),
+  };
+  const float64 = xyEnsureFloat64(data);
+  float64.x[0] = 0;
+  expect(float64.x).toBeInstanceOf(Float64Array);
+  expect(float64.y).toBeInstanceOf(Float64Array);
+  expect(data.x).toStrictEqual(Float64Array.from([-1, 2, -3, 4]));
+  expect(float64.x).toStrictEqual(Float64Array.from([0, 2, -3, 4]));
+  expect(float64.y).toStrictEqual(Float64Array.from([-1, 2, -3, 4]));
+});
