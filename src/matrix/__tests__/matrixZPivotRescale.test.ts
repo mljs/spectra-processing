a7df5f70d5d81d602e397d340749b97901c7a99a
jobo322 <jose.bolanos@correounivalle.edu.co>
2024-03-07 02:45:56 -0500

    fix: matrixZPivotRescale can use NumberArrayConstructors

--- src/matrix/__tests__/matrixZPivotRescale.test.ts
+++ src/matrix/__tests__/matrixZPivotRescale.test.ts
@@ -31,3 +31,21 @@ test('with negative larger number', () => {
     [-30, 10, -30, 10],
   ]);
 });
+
+test('only positive number in Int16 output', () => {
+  const data = [
+    [1, 3, 2, 2],
+    [2, 2, 1, 3],
+    [3, 1, 3, 1],
+  ];
+  const result = matrixZPivotRescale(data, {
+    max: 30,
+    ArrayConstructor: Int16Array,
+  });
+
+  expect(result).toStrictEqual([
+    Int16Array.from([10, 30, 20, 20]),
+    Int16Array.from([20, 20, 10, 30]),
+    Int16Array.from([30, 10, 30, 10]),
+  ]);
+});
