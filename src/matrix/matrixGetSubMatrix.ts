97635254f5277bee908d9c3cd6ae1654677f057d
jobo322 <jose.bolanos@correounivalle.edu.co>
2024-03-21 15:49:03 -0500

    feat: add matrixGetSubMatrix function

---
+++ src/matrix/matrixGetSubMatrix.ts
@@ -0,0 +1,34 @@
+import { DoubleMatrix } from '../types';
+import { matrixCreateEmpty } from './index';
+import { matrixCheckRanges } from './matrixCheckRanges';
+
+/**
+ * Get a subMatrix from matrix, the function check if the subMatrix
+ * lies into the dimensions of matrix.
+ * @param matrix - matrix that will receive the new element values.
+ * @param startRow - row index in matrix for the first row in subMatrix.
+ * @param startColumn - column index in matrix for the first column in subMatrix.
+ * @returns The modified `matrix`.
+ */
+export function matrixGetSubMatrix(
+  matrix: DoubleMatrix,
+  options: {
+    startRow: number;
+    startColumn: number;
+    endRow: number;
+    endColumn: number;
+  },
+): DoubleMatrix {
+  matrixCheckRanges(matrix, options);
+  const { startRow, endRow, startColumn, endColumn } = options;
+  const nbColumns = endColumn - startColumn + 1;
+  const nbRows = endRow - startRow + 1;
+
+  const subMatrix = matrixCreateEmpty({ nbColumns, nbRows });
+  for (let i = 0; i < nbRows; i++) {
+    for (let j = 0; j < nbColumns; j++) {
+      subMatrix[i][j] = matrix[startRow + i][startColumn + j];
+    }
+  }
+  return subMatrix;
+}
