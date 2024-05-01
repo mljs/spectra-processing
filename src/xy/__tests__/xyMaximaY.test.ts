0adcf7099e91ce52b451065d4a19f483ea518a19
Luc Patiny <luc@patiny.com>
2024-05-01 15:30:19 +0200

    feat: allow from / to in xyMaximaY and xyMinimaY

--- src/xy/__tests__/xyMaximaY.test.ts
+++ src/xy/__tests__/xyMaximaY.test.ts
@@ -10,3 +10,11 @@ test('clear top', () => {
     { x: 5, y: 3, index: 4 },
   ]);
 });
+
+test('clear top and from to', () => {
+  const x = [1, 2, 3, 4, 5, 6];
+  const y = [2, 3, 1, 2, 3, 2];
+  expect(xyMaximaY({ x, y }, { from: 3 })).toStrictEqual([
+    { x: 5, y: 3, index: 4 },
+  ]);
+});
--- src/xy/xyMinimaY.ts
+++ src/xy/xyMinimaY.ts
@@ -1,6 +1,7 @@
 import { DataXY } from 'cheminfo-types';
 
 import { PointWithIndex } from '../types';
+import { xGetFromToIndex, XGetFromToIndexOptions } from '../x/xGetFromToIndex';
 
 import { xyCheck } from './xyCheck';
 
@@ -10,14 +11,19 @@ import { xyCheck } from './xyCheck';
  * of the equal part will be the position of the signal!
  *
  * @param data - Object that contains property X (an ordered increasing array) and y (an arraY)
+ * @param options - Object with from and to properties
  * @returns - Array of points.
  */
-export function xyMinimaY(data: DataXY): PointWithIndex[] {
+export function xyMinimaY(
+  data: DataXY,
+  options: XGetFromToIndexOptions = {},
+): PointWithIndex[] {
   xyCheck(data, { minLength: 2 });
   const { x, y } = data;
+  const { fromIndex, toIndex } = xGetFromToIndex(x, options);
   const maxima = [];
   let startEqualIndex = -1;
-  for (let i = 1; i < x.length - 1; i++) {
+  for (let i = fromIndex + 1; i < toIndex; i++) {
     if (y[i - 1] > y[i] && y[i + 1] > y[i]) {
       maxima.push({ x: x[i], y: y[i], index: i });
     } else if (y[i - 1] > y[i] && y[i + 1] === y[i]) {
