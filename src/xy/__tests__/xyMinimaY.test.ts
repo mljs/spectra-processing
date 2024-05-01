f5ff00169fb16b94adc93e117a3931189eff90ee
Luc Patiny <lpatiny@users.noreply.github.com>
2024-05-01 15:33:19 +0200

    fromToMaxima (#241)
    * chore: update dependencies
    
    * feat: allow from / to in xyMaximaY and xyMinimaY

--- src/xy/__tests__/xyMinimaY.test.ts
+++ src/xy/__tests__/xyMinimaY.test.ts
@@ -2,11 +2,19 @@ import { expect, test } from 'vitest';
 
 import { xyMinimaY } from '../xyMinimaY';
 
-test('clear top', () => {
+test('clear bottom', () => {
   const x = [1, 2, 3, 4, 5, 6];
   const y = [-2, -3, -1, -2, -3, -2];
   expect(xyMinimaY({ x, y })).toStrictEqual([
     { x: 2, y: -3, index: 1 },
     { x: 5, y: -3, index: 4 },
+  ]);
+});
+
+test('clear bottom with from to', () => {
+  const x = [1, 2, 3, 4, 5, 6];
+  const y = [-2, -3, -1, -2, -3, -2];
+  expect(xyMinimaY({ x, y }, { from: 1, to: 5 })).toStrictEqual([
+    { x: 2, y: -3, index: 1 },
   ]);
 });
--- src/xy/xyMaximaY.ts
+++ src/xy/xyMaximaY.ts
@@ -1,6 +1,7 @@
 import { DataXY } from 'cheminfo-types';
 
 import { PointWithIndex } from '../types';
+import { xGetFromToIndex, XGetFromToIndexOptions } from '../x/xGetFromToIndex';
 
 import { xyCheck } from './xyCheck';
 
@@ -10,14 +11,19 @@ import { xyCheck } from './xyCheck';
  * of the equal part will be the position of the signal!
  *
  * @param data - Object that contains property x (an ordered increasing array) and y (an array)
+ * @param options - Object with from and to properties
  * @returns - Array of points
  */
-export function xyMaximaY(data: DataXY): PointWithIndex[] {
+export function xyMaximaY(
+  data: DataXY,
+  options: XGetFromToIndexOptions = {},
+): PointWithIndex[] {
   xyCheck(data, { minLength: 2 });
   const { x, y } = data;
+  const { fromIndex, toIndex } = xGetFromToIndex(x, options);
   const maxima: PointWithIndex[] = [];
   let startEqualIndex = -1;
-  for (let i = 1; i < x.length - 1; i++) {
+  for (let i = fromIndex + 1; i < toIndex; i++) {
     if (y[i - 1] < y[i] && y[i + 1] < y[i]) {
       maxima.push({ x: x[i], y: y[i], index: i });
     } else if (y[i - 1] < y[i] && y[i + 1] === y[i]) {
