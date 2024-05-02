bd4679631b33be189255ee64b5cf18fff38efb2b
Jose Alejandro Bolanos Arroyave <jose.bolanos@correounivalle.edu.co>
2024-05-02 10:59:58 -0500

    feat: add xAbsoluteSum method (#245)

---
+++ src/x/__tests__/xAbsoluteSum.test.ts
@@ -0,0 +1,21 @@
+import { expect, test } from 'vitest';
+
+import { xAbsoluteSum } from '../index';
+
+test('normal array', () => {
+  const array = [-1, 2, -3, 4];
+  expect(xAbsoluteSum(array)).toStrictEqual(10);
+});
+
+test('typed array', () => {
+  const array = new Float64Array([-1, 2, -3, 4]);
+  expect(xAbsoluteSum(array)).toBe(10);
+});
+
+test('typed array from to', () => {
+  const array = new Float64Array([10, -1, 2, -3, 4, 20]);
+
+  const fromIndex = 1;
+  const toIndex = 4;
+  expect(xAbsoluteSum(array, { fromIndex, toIndex })).toBe(10);
+});
