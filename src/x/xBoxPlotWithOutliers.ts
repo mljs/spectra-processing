038a57686439f68cfc05aa0240b97c7e6ca220fe
Luc Patiny <luc@patiny.com>
2024-10-01 13:52:31 +0200

    feat: add xBoxPlotWithOutliers mehtod to deal with possible outliers
    This removes the previous option in xBoxPlot that would have yield to different types

---
+++ src/x/xBoxPlotWithOutliers.ts
@@ -0,0 +1,43 @@
+import { NumberArray } from 'cheminfo-types';
+
+import { xBoxPlot, XBoxPlotOptions } from './xBoxPlot';
+
+export interface XBoxPlotWithOutliers {
+  q1: number;
+  median: number;
+  q3: number;
+  min: number;
+  max: number;
+  outliers: number[];
+}
+
+/**
+ * Calculating the box plot of the array
+ * @param array - data
+ * @param options
+ */
+export function xBoxPlotWithOutliers(
+  array: NumberArray,
+  options: XBoxPlotOptions = {},
+): XBoxPlotWithOutliers {
+  const info: XBoxPlotWithOutliers = {
+    ...xBoxPlot(array, options),
+    outliers: [],
+  };
+
+  const iqr = info.q3 - info.q1;
+  const min = info.q1 - 1.5 * iqr;
+  const max = info.q3 + 1.5 * iqr;
+  // we need to recalculate the min and the max because they could be outliers
+  info.min = info.median;
+  info.max = info.median;
+  for (const value of array) {
+    if (value < min || value > max) {
+      info.outliers.push(value);
+    } else {
+      if (value < info.min) info.min = value;
+      if (value > info.max) info.max = value;
+    }
+  }
+  return info;
+}
