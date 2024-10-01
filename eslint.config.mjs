6e0750d173581231c4e8b7c4eced39bf11f22273
Luc Patiny <luc@patiny.com>
2024-10-01 10:39:37 +0200

    chore disable lines-before-block eslint rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,8 +10,7 @@ export default [
       },
     },
     rules: {
-      "jsdoc/require-jsdoc": "off", // this would add automatically an empty bloc of JsDoc
-      "jsdoc/no-defaults": "off", // this would remove our default values https://github.com/gajus/eslint-plugin-jsdoc/blob/main/docs/rules/no-defaults.md#readme
+      "jsdoc/lines-before-block": "off",
       "no-loss-of-precision": "off",
       '@typescript-eslint/prefer-for-of': 'off',
       "unicorn/import-style": [
