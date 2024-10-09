5251c79af304f0c9188053059dab527367d5b8f3
jobo322 <jose.bolanos@correounivalle.edu.co>
2024-10-09 11:48:23 -0500

    chore: add interface for matrixGetSubMatrix options

--- src/matrix/__tests__/matrixGetSubMatrix.test.ts
+++ src/matrix/__tests__/matrixGetSubMatrix.test.ts
@@ -1,33 +1,31 @@
-import { matrixGetSubMatrix } from '../index';
+import { matrixGetSubMatrix } from '../matrixGetSubMatrix';
 
-describe('matrixGetSubMatrix', () => {
-  const matrix = [
-    new Float64Array([1, 2, 3]),
-    new Float64Array([4, 5, 6]),
-    new Float64Array([7, 8, 9]),
-  ];
+const matrix = [
+  new Float64Array([1, 2, 3]),
+  new Float64Array([4, 5, 6]),
+  new Float64Array([7, 8, 9]),
+];
 
-  it('should extract submatrix correctly', () => {
-    const options = {
-      startRow: 0,
-      startColumn: 1,
-      endRow: 1,
-      endColumn: 2,
-    };
+test('should extract submatrix correctly', () => {
+  const options = {
+    startRow: 0,
+    startColumn: 1,
+    endRow: 1,
+    endColumn: 2,
+  };
 
-    const subMatrix = matrixGetSubMatrix(matrix, options);
-    const expectedResult = [new Float64Array([2, 3]), new Float64Array([5, 6])];
-    expect(subMatrix).toEqual(expectedResult);
-  });
+  const subMatrix = matrixGetSubMatrix(matrix, options);
+  const expectedResult = [new Float64Array([2, 3]), new Float64Array([5, 6])];
+  expect(subMatrix).toEqual(expectedResult);
+});
 
-  it('should throw RangeError for out-of-range indices', () => {
-    const options = {
-      startRow: 0,
-      startColumn: 1,
-      endRow: 3, // Out of range
-      endColumn: 2,
-    };
+test('should throw RangeError for out-of-range indices', () => {
+  const options = {
+    startRow: 0,
+    startColumn: 1,
+    endRow: 3, // Out of range
+    endColumn: 2,
+  };
 
-    expect(() => matrixGetSubMatrix(matrix, options)).toThrow(RangeError);
-  });
+  expect(() => matrixGetSubMatrix(matrix, options)).toThrow(RangeError);
 });
