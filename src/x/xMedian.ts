f37808a710d443645a9b209722b8ea18bcf9ea90
jobo322 <jose.bolanos@correounivalle.edu.co>
2025-02-11 23:26:38 -0500

    refactor: replace input validation with xCheck in xMedian function

--- src/x/xMedian.ts
+++ src/x/xMedian.ts
@@ -1,5 +1,6 @@
 import type { NumberArray } from 'cheminfo-types';
-import { isAnyArray } from 'is-any-array';
+
+import { xCheck } from './xCheck';
 
 /**
  * Calculates the median of an array.
@@ -7,13 +8,7 @@ import { isAnyArray } from 'is-any-array';
  * @returns - median
  */
 export function xMedian(input: NumberArray): number {
-  if (!isAnyArray(input)) {
-    throw new TypeError('input must be an array');
-  }
-
-  if (input.length === 0) {
-    throw new TypeError('input must not be empty');
-  }
+  xCheck(input);
 
   const array = input.slice();
 
