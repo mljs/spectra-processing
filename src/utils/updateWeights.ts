3cae4457605df9da4c08600ea4a639c931696cf5
jobo322 <jose.bolanos@correounivalle.edu.co>
2025-02-20 11:05:23 -0500

    feat: enhance UpdateWeightsOptions interface with weights and control points

--- src/utils/updateWeights.ts
+++ src/utils/updateWeights.ts
@@ -4,11 +4,37 @@ import { xAbsolute } from '../x/xAbsolute';
 import { xMedian } from '../x/xMedian';
 import { xSubtract } from '../x/xSubtract';
 
-interface UpdateWeightsOptions {
+export interface WeightsAndControlPoints {
+  /**
+   * Control points to determine which weights to update.
+   */
   controlPoints?: NumberArray;
+  /**
+   * Array of weights
+   * @default [1,1,...,1]
+   */
+  weights?: NumberArray;
+}
+
+export interface UpdateWeightsOptions extends WeightsAndControlPoints {
+  /**
+   * Factor that determines how quickly the weights are updated in each iteration.
+   * A value between 0 and 1, where higher values mean faster updates.
+   * @default 0.5
+   */
   learningRate: number;
+  /**
+   * The minimum allowed weight value to prevent weights from becoming too small.
+   * @default 0.01
+   */
   minWeight: number;
-  residualFactor: number;
+  /**
+   * Factor used to calculate the threshold for determining outliers in the residuals.
+   * Higher values mean more tolerance for outliers. The default value is based on noise follow the normal distribution
+   * values over 3 times the standard-deviation could be marked as signals or outliers.
+   * @default 3
+   */
+  factorStd: number;
 }
 
 /**
@@ -16,11 +42,7 @@ interface UpdateWeightsOptions {
  * @param yData - The original data.
  * @param newBaseline - The new baseline calculated.
  * @param weights - The current weights to be updated.
- * @param options - Options for updating weights.
- * @param options.learningRate - The learning rate for updating weights.
- * @param options.minWeight - The minimum weight value.
- * @param options.residualFactor - The factor to calculate the threshold for residuals.
- * @param options.controlPoints - Control points to determine which weights to update.
+ * @param options - Options for updating weights in the Whittaker smoothing algorithm.
  * @returns The updated weights.
  */
 
@@ -30,12 +52,12 @@ export function updateWeights(
   weights: NumberArray,
   options: UpdateWeightsOptions,
 ) {
-  const { controlPoints, residualFactor, learningRate, minWeight } = options;
+  const { controlPoints, factorStd, learningRate, minWeight } = options;
   const absResiduals = xAbsolute(xSubtract(yData, newBaseline));
 
   const medAbsRes = xMedian(absResiduals);
   const mad = 1.4826 * medAbsRes;
-  const threshold = residualFactor * mad;
+  const threshold = factorStd * mad;
 
   const rawWeights = new Float64Array(absResiduals.length);
   for (let i = 0; i < absResiduals.length; i++) {
