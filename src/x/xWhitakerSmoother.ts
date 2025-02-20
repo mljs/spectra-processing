6d5e17a107982acc10394021429db2435c2b6f44
jobo322 <jose.bolanos@correounivalle.edu.co>
2025-02-20 11:05:23 -0500

    feat: enhance UpdateWeightsOptions interface with weights and control points

--- src/x/xWhitakerSmoother.ts
+++ src/x/xWhitakerSmoother.ts
@@ -3,12 +3,13 @@ import type { NumberArray } from 'cheminfo-types';
 import { matrixCholeskySolver } from '../matrix/matrixCholeskySolver';
 import { addWeights } from '../utils/addWeights';
 import { createSystemMatrix } from '../utils/createSystemMatrix';
+import type { WeightsAndControlPoints } from '../utils/updateWeights';
 import { updateWeights } from '../utils/updateWeights';
 
 import { xEnsureFloat64 } from './xEnsureFloat64';
 import { xMultiply } from './xMultiply';
 
-interface XWhitakerSmootherOptions extends ControlPointsOptions {
+interface XWhitakerSmootherOptions extends WeightsAndControlPoints {
   /**
    * Factor of weights matrix in -> [I + lambda D'D]z = x
    * @default 100
@@ -48,7 +49,7 @@ interface XWhitakerSmootherOptions extends ControlPointsOptions {
 /**
  * Computes the baseline points for the given data using an iterative smoothing algorithm.
  * @param yData - The input data array.
- * @param [options={}] - The options for baseline computation.
+ * @param options - The options for baseline computation.
  * @returns - The computed baseline points.
  */
 export function xWhitakerSmoother(
@@ -66,7 +67,7 @@ export function xWhitakerSmoother(
 
   const size = yData.length;
 
-  const { controlPoints, weights } = getControlPoints(yData, options);
+  const { controlPoints, weights } = getWeightsAndControlPoints(yData, options);
   const prevBaseline: Float64Array = new Float64Array(size);
 
   let iteration = 0;
@@ -123,20 +124,15 @@ function calculateDelta(
   return Math.sqrt(sum / n);
 }
 
-interface ControlPointsOptions {
-  controlPoints?: NumberArray;
-  weights?: NumberArray;
-}
-
 /**
- * Retrieves the control points and weights for the given data.
+ * Retrieves the control points and weights for the given data, the weights are modified multiplication of controlPoints if it exists.
  * @param  y - The input data array.
- * @param  [options={}] - The options for control points and weights.
+ * @param  options - The options for control points and weights.
  * @returns - The control points and weights.
  */
-function getControlPoints(
+function getWeightsAndControlPoints(
   y: NumberArray,
-  options: ControlPointsOptions = {},
+  options: WeightsAndControlPoints = {},
 ): { weights: NumberArray; controlPoints?: NumberArray } {
   const { length } = y;
   const { controlPoints } = options;
