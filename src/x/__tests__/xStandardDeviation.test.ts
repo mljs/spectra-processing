96a09967e834f192ff055e30f1f73885a59d07f4
Luc Patiny <lpatiny@users.noreply.github.com>
2025-04-12 06:25:55 +0200

    test: add more tests for xStandardDeviation (#298)

--- src/x/__tests__/xStandardDeviation.test.ts
+++ src/x/__tests__/xStandardDeviation.test.ts
@@ -2,15 +2,8 @@ import { expect, test } from 'vitest';
 
 import { xStandardDeviation } from '../xStandardDeviation';
 
-const data = [15, 13, 17, 7];
-
-const typedArray = new Uint16Array(4);
-typedArray[0] = 15;
-typedArray[1] = 13;
-typedArray[2] = 17;
-typedArray[3] = 7;
-
 test('standard deviation', () => {
+  const data = [15, 13, 17, 7];
   const s = xStandardDeviation(data);
   expect(s).toBeCloseTo(Math.sqrt(18.667), 3);
   expect(xStandardDeviation(data, { unbiased: true })).toBe(s);
@@ -18,8 +11,21 @@ test('standard deviation', () => {
 });
 
 test('standard deviation of typed array', () => {
+  const data = [15, 13, 17, 7];
+  const typedArray = Uint16Array.from(data);
   const s = xStandardDeviation(typedArray);
   expect(s).toBeCloseTo(Math.sqrt(18.667), 3);
   expect(xStandardDeviation(data, { unbiased: true })).toBe(s);
   expect(xStandardDeviation(data, { unbiased: false })).toBe(Math.sqrt(14));
+});
+
+test('one element', () => {
+  const data = [15];
+  expect(xStandardDeviation(data)).toBe(Number.NaN);
+  expect(xStandardDeviation(data, { unbiased: false })).toBe(0);
+});
+
+test('empty array', () => {
+  const data: number[] = [];
+  expect(() => xStandardDeviation(data)).toThrow('input must not be empty');
 });
