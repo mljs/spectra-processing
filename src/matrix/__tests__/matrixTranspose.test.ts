f0c04d14e7e47d234a5074dd81090b1d3594f103
jobo322 <jose.bolanos@correounivalle.edu.co>
2025-07-23 07:43:52 -0500

    feat: add matrixTranspose function

---
+++ src/matrix/__tests__/matrixTranspose.test.ts
@@ -0,0 +1,42 @@
+import { test, expect } from 'vitest';
+
+import { matrixTranspose } from '../matrixTranspose';
+
+test('should transpose a square matrix', () => {
+  const matrix = [
+    [1, 2],
+    [3, 4],
+  ];
+  expect(matrixTranspose(matrix, { ArrayConstructor: Array })).toEqual([
+    [1, 3],
+    [2, 4],
+  ]);
+});
+
+test('should transpose a rectangular matrix', () => {
+  const matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+  ];
+  expect(matrixTranspose(matrix, { ArrayConstructor: Array })).toEqual([
+    [1, 4],
+    [2, 5],
+    [3, 6],
+  ]);
+});
+
+test('should transpose a single row matrix', () => {
+  const matrix = [[1, 2, 3]];
+  expect(matrixTranspose(matrix, { ArrayConstructor: Array })).toEqual([
+    [1],
+    [2],
+    [3],
+  ]);
+});
+
+test('should transpose a single column matrix', () => {
+  const matrix = [[1], [2], [3]];
+  expect(matrixTranspose(matrix, { ArrayConstructor: Array })).toEqual([
+    [1, 2, 3],
+  ]);
+});
---
+++ src/matrix/matrixTranspose.ts
@@ -0,0 +1,39 @@
+import type { NumberArrayConstructor } from '../utils';
+
+import { matrixCreateEmpty } from './matrixCreateEmpty';
+
+export interface MatrixTransposeOptions<
+  ArrayConstructorType extends NumberArrayConstructor = Float64ArrayConstructor,
+> {
+  /**
+   * Allows to specify the type of array to use
+   * @default Float64Array
+   */
+  ArrayConstructor?: ArrayConstructorType;
+}
+
+export function matrixTranspose<
+  ArrayConstructorType extends NumberArrayConstructor = Float64ArrayConstructor,
+>(
+  matrix: number[][],
+  options: MatrixTransposeOptions<ArrayConstructorType> = {},
+) {
+  const { ArrayConstructor } = options;
+  const nbRows = matrix.length;
+  const nbColumns = matrix[0].length;
+
+  // Create new matrix with swapped dimensions
+  const result = matrixCreateEmpty({
+    nbColumns: nbRows,
+    nbRows: nbColumns,
+    ArrayConstructor,
+  });
+
+  for (let i = 0; i < nbRows; i++) {
+    for (let j = 0; j < nbColumns; j++) {
+      result[j][i] = matrix[i][j];
+    }
+  }
+
+  return result;
+}
