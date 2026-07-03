3e17e80ddd4fc93d973c48003e4fc77704385fac
Luc Patiny <luc@patiny.com>
2026-07-03 11:06:34 +0200

    chore: seems the fix was just to use lowerWhisker and upperWhisker and not to create a Set

--- src/x/xRobustDistributionStats.ts
+++ src/x/xRobustDistributionStats.ts
@@ -39,10 +39,9 @@ export function xRobustDistributionStats(
     filteredArray = array;
   } else {
     filteredArray = new Float64Array(array.length - boxPlot.outliers.length);
-    const outliersSet = new Set(boxPlot.outliers);
     let j = 0;
     for (const element of array) {
-      if (!outliersSet.has(element)) {
+      if (element >= boxPlot.lowerWhisker && element <= boxPlot.upperWhisker) {
         filteredArray[j++] = element;
       }
     }
